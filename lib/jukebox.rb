458ae08b75b7d454b4cb795a540299caaf95443e
Romario Gustave <romagustave10@gmail.com>
2019-10-22 04:30:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,5 @@
 # Add your code here
-def music(songs)
+def music
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -13,4 +13,12 @@ songs = [
 ]
 end 
 def help 
-  puts 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+def list(songs)
+  array = 
+  
