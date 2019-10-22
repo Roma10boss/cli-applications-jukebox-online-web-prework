8a1c603ea27403315b80a1449b0000ab7da2ee91
Romario Gustave <romagustave10@gmail.com>
2019-10-22 04:44:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,11 +36,28 @@ def play(songs)
   puts "Please enter a song name or number:"
    num = number.gets.strip
    if num = array[0] 
-     puts "playin the song " + array[1] 
-     elsif num = array[0] 
+     puts "playin the song " + array[0] 
+     elsif num = array[1] 
      puts "playin the song " + array[1]
+     elsif num = array[2] 
+     puts "playin the song " + array[2] 
+     elsif num = array[3] 
+     puts "playin the song " + array[3] 
+     elsif num = array[4] 
+     puts "playin the song " + array[4] 
+     elsif num = array[5] 
+     puts "playin the song " + array[5] 
+     elsif num = array[6] 
+     puts "playin the song " + array[6] 
+     elsif num = array[7] 
+     puts "playin the song " + array[] 
+     elsif num = array[8] 
+     puts "playin the song " + array[8]
+     elsif num = array[9] 
+     puts "playin the song " + array[9]
      
-     
+   else 
+     puts 
  end
  puts play(num)
  
